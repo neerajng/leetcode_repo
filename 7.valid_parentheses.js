2d37a8ffd05326be2fe2011b2d50ba1f0fdec20d
neerajng <neerajnrjng@gmail.com>
2023-12-24 05:14:51 +0530

    7.valid_parentheses

---
+++ 7.valid_parentheses.js
@@ -0,0 +1,30 @@
+//Valid Parentheses
+//Given a string s containing just the characters 
+//'(', ')', '{', '}', '[' and ']', determine if the input string is valid.
+
+
+const isValid = function(s){
+    let stack=[];
+
+    for(let i=0;i<s.length;i++){
+        let c=s[i]
+
+        switch(c){
+            case '(' : stack.push(')')
+            break;
+            case '{' : stack.push('}')
+            break;
+            case '[' : stack.push(']');
+            break;
+            default:
+                if(c !==stack.pop()){
+                    return false
+                }
+        }
+    }
+
+    return stack.length===0
+}
+
+const string = "("
+console.log(isValid(string))
