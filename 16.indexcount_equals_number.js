f48a463504a39810c47e6a255cc040aca2cd14fd
neerajng <neerajnrjng@gmail.com>
2024-01-04 10:50:21 +0530

    16.indexcount_equals_number

---
+++ 16.indexcount_equals_number.js
@@ -0,0 +1,12 @@
+const digitCount = function(num) {
+    let arr=Array(num.length).fill(0) 
+    for (let char of num){
+       arr[Number(char)]++ 
+    }
+    return arr.join('')===num  
+};
+
+
+
+const _num = '1210'
+console.log(digitCount(_num)) 
