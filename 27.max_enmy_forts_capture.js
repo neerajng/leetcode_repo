1e9e04e4c8b9f1dc47005b3e84c9c1f3beefb1d5
neerajng <neerajnrjng@gmail.com>
2024-01-15 10:14:34 +0530

    27.max_enmy_forts_capture

--- 27.max_enmy_forts_capture.js
+++ 27.max_enmy_forts_capture.js
@@ -1,20 +1,15 @@
-var captureForts = function(forts) {
-    let res=0
-    for(let i=0,j=0;i<forts.length;i++){
-
-        if(forts[i]!=0){//1
-
-            if(forts[j]== -forts[i]){//1==--1
-                res=Math.max(res, i-j-1);//3-1-1=1
-            }
-
-        j=i;//j=1
-        }
+var captureForts = function (forts) {
+  let res = 0;
+  for (let i = 0, j = 0; i < forts.length; i++) {
+    if (forts[i] != 0) {
+      if (forts[j] == -forts[i]) {
+        res = Math.max(res, i - j - 1);
+      }
+      j = i;
     }
-    return res;
-};  
-    
+  }
+  return res;
+};
 
-
-const forts = [0,1,0,-1]
+const forts = [0, 1, 0, -1];
 console.log(captureForts(forts));
