4403c3c2905dd63a2d096be7fdc3671cf64e126a
neerajng <neerajnrjng@gmail.com>
2024-01-22 14:27:38 +0530

    40.find_max_str_pair.js

--- 40.find_max_str_pair.js
+++ 40.find_max_str_pair.js
@@ -1,1 +1,16 @@
-ECHO is on.
+const maximumNumberOfStringPairs = function(words) {
+    let obj={}, count=0
+    for(let word of words){
+        let pair=word.split('').sort().join('')
+        if(!obj[pair]){
+            obj[pair]=1
+        }else if(obj[pair]===1){
+            count++
+        }
+    }
+    return count
+    
+};
+
+const words = ["aa","ab"]
+console.log(maximumNumberOfStringPairs(words));
