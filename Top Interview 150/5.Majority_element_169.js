bb3567390c1819ec46b27b1e99eacc4243460307
neerajng <neerajnrjng@gmail.com>
2024-06-11 17:31:57 +0530

    Majority-element-169

---
+++ Top Interview 150/5.Majority_element_169.js
@@ -0,0 +1,11 @@
+const majorityElement =  (nums)=> {
+    let obj={}
+    for(let num of nums){        
+     (!obj[num])?obj[num]=1:++obj[num]; 
+     if(obj[num]>nums.length/2) return num;
+    }
+}
+
+const nums = [2,2,1,1,1,2,2];
+
+console.log(majorityElement(nums));
